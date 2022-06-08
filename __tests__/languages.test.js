7650b45f013461ad9ed5e1204e9c447a0ddf255b
Francisco Marquez <fmarquez@griddynamics.com>
2022-06-08 12:15:47 -0500

    Added env variable for timezone consistency, changed naming conventions

--- __tests__/languages.test.js
+++ __tests__/languages.test.js
@@ -12,119 +12,119 @@ let testDate = new Date("June 17, 2022 17:24:12 GMT-0500");
 
 describe("Date formating be", () => {
   beforeAll(() => unitTestingTask.lang("be", be));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("чэрвень");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("чэр");
   });
-  test("formats date to AM/PM", () => {
+  it("should format date to AM/PM", () => {
     expect(unitTestingTask("A", testDate)).toBe("вечара");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("пятніца");
   });
 });
 
 describe("Date formating cs", () => {
   beforeAll(() => unitTestingTask.lang("cs", cs));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("červen");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("čvn");
   });
-  test("formats date to AM/PM", () => {
+  it("should format date to AM/PM", () => {
     expect(unitTestingTask("A", testDate)).toBe("odpoledne");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("pátek");
   });
 });
 
 describe("Date formating kk", () => {
   beforeAll(() => unitTestingTask.lang("kk", kk));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("маусым");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("маус");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("жұма");
   });
 });
 
 describe("Date formating pl", () => {
   beforeAll(() => unitTestingTask.lang("pl", pl));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("czerwiec");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("cze");
   });
-  test("formats date to AM/PM", () => {
+  it("should format date to AM/PM", () => {
     expect(unitTestingTask("A", testDate)).toBe("");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("piątek");
   });
 });
 
 describe("Date formating ru", () => {
   beforeAll(() => unitTestingTask.lang("ru", ru));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("июнь");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("июнь");
   });
-  test("formats date to AM/PM", () => {
+  it("should format date to AM/PM", () => {
     expect(unitTestingTask("A", testDate)).toBe("вечера");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("пятница");
   });
 });
 
 describe("Date formating tr", () => {
   beforeAll(() => unitTestingTask.lang("tr", tr));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("Haziran");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("Haz");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("Cuma");
   });
 });
 
 describe("Date formating tt", () => {
   beforeAll(() => unitTestingTask.lang("tt", tt));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("июнь");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("июнь");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("җомга");
   });
 });
 
 describe("Date formating uk", () => {
   beforeAll(() => unitTestingTask.lang("uk", uk));
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("червень");
   });
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("черв");
   });
-  test("formats date to AM/PM", () => {
+  it("should format date to AM/PM", () => {
     expect(unitTestingTask("A", testDate)).toBe("вечора");
   });
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("п’ятниця");
   });
 });
--- __tests__/unitTesting.test.js
+++ __tests__/unitTesting.test.js
@@ -1,129 +1,135 @@
 const unitTestingTask = require("../unitTestingTask.js");
 
-let testDate = new Date("June 17, 2022 17:24:12 GMT-0500");
+let testDate = new Date("June 17, 2022 17:24:12 GMT-0000");
+
+describe("Timezones", () => {
+  it("should always be UTC", () => {
+    expect(new Date().getTimezoneOffset()).toBe(0);
+  });
+});
 
 describe("Date formating english", () => {
-  test("formats date to 4 digit year", () => {
+  it("should format date to 4 digit year", () => {
     expect(unitTestingTask("YYYY", testDate)).toBe("2022");
   });
 
-  test("formats date to last 2 digit of year", () => {
+  it("should format date to last 2 digit of year", () => {
     expect(unitTestingTask("YY", testDate)).toBe("22");
   });
 
-  test("formats date to full name of month", () => {
+  it("should format date to full name of month", () => {
     expect(unitTestingTask("MMMM", testDate)).toBe("June");
   });
 
-  test("formats date to short name of month", () => {
+  it("should format date to short name of month", () => {
     expect(unitTestingTask("MMM", testDate)).toBe("Jun");
   });
 
-  test("formats date to number of month zero-padded", () => {
+  it("should format date to number of month zero-padded", () => {
     expect(unitTestingTask("MM", testDate)).toBe("06");
   });
 
-  test("formats date to number of month in year", () => {
+  it("should format date to number of month in year", () => {
     expect(unitTestingTask("M", testDate)).toBe("6");
   });
 
-  test("formats date to full name of day", () => {
+  it("should format date to full name of day", () => {
     expect(unitTestingTask("DDD", testDate)).toBe("Friday");
   });
 
-  test("formats date to short name of day", () => {
+  it("should format date to short name of day", () => {
     expect(unitTestingTask("DD", testDate)).toBe("Fri");
   });
 
-  test("formats date to min name of day", () => {
+  it("should format date to min name of day", () => {
     expect(unitTestingTask("D", testDate)).toBe("Fr");
   });
 
-  test("formats date to zero-padded number of day in month", () => {
+  it("should format date to zero-padded number of day in month", () => {
     expect(unitTestingTask("dd", testDate)).toBe("17");
   });
-  test("formats date to month number of day in month", () => {
+  it("should format date to month number of day in month", () => {
     expect(unitTestingTask("d", testDate)).toBe("17");
   });
 
-  test("formats date to zero-padded hour in 24hr format", () => {
+  it("should format date to zero-padded hour in 24hr format", () => {
     expect(unitTestingTask("HH", testDate)).toBe("17");
   });
 
-  test("formats date to hour in 24hr format", () => {
+  it("should format date to hour in 24hr format", () => {
     expect(unitTestingTask("H", testDate)).toBe("17");
   });
 
-  test("formats date to zero-padded hour in 12hr format", () => {
+  it("should format date to zero-padded hour in 12hr format", () => {
     expect(unitTestingTask("hh", testDate)).toBe("05");
   });
 
-  test("formats date to hour in 12hr format", () => {
+  it("should format date to hour in 12hr format", () => {
     expect(unitTestingTask("h", testDate)).toBe("5");
   });
 
-  test("formats date to zero-padded minutes", () => {
+  it("should format date to zero-padded minutes", () => {
     expect(unitTestingTask("mm", testDate)).toBe("24");
   });
 
-  test("formats date to minutes", () => {
+  it("should format date to minutes", () => {
     expect(unitTestingTask("m", testDate)).toBe("24");
   });
 
-  test("formats date to zero-padded seconds", () => {
+  it("should format date to zero-padded seconds", () => {
     expect(unitTestingTask("ss", testDate)).toBe("12");
   });
 
-  test("formats date to seconds", () => {
+  it("should format date to seconds", () => {
     expect(unitTestingTask("s", testDate)).toBe("12");
   });
 
-  test("formats date to zero-padded miliseconds", () => {
+  it("should format date to zero-padded miliseconds", () => {
     expect(unitTestingTask("ff", testDate)).toBe("000");
   });
-  test("formats date to miliseconds", () => {
+  it("should format date to miliseconds", () => {
     expect(unitTestingTask("f", testDate)).toBe("0");
   });
 
-  test("formats date to AM/PM", () => {
+  it("should format date to AM/PM", () => {
     expect(unitTestingTask("A", testDate)).toBe("PM");
   });
 
-  test("formats date to am/pm", () => {
+  it("should format date to am/pm", () => {
     expect(unitTestingTask("a", testDate)).toBe("pm");
   });
 
-  test("formats date to time-zone", () => {
-    expect(unitTestingTask("ZZ", testDate)).toBe("-0500");
+  it("should format date to time-zone", () => {
+    expect(unitTestingTask("ZZ", testDate)).toBe("+0000");
   });
 
-  test("formats date to time-zone extended", () => {
-    expect(unitTestingTask("Z", testDate)).toBe("-05:00");
+  it("should format date to time-zone extended", () => {
+    expect(unitTestingTask("Z", testDate)).toBe("+00:00");
   });
 
-  test("formats date to day month year", () => {
-    expect(unitTestingTask("DDDMMMYYYY", testDate)).toBe("FridayJun2022");
+  it("should format date to day month year", () => {
+    expect(unitTestingTask("DDD MMM YYYY", testDate)).toBe("Friday Jun 2022");
   });
 
-  test("formats date to 12 hr format with AM/PM", () => {
+  it("should format date to 12 hr format with AM/PM", () => {
     expect(unitTestingTask("hA", testDate)).toBe("5PM");
   });
 
-  test("formats date to 12 hr format with AM/PM", () => {
+  it("should format date to ISODateTimeTZ (YYYY-MM-ddThh:mm:ss)", () => {
     expect(unitTestingTask("ISODateTimeTZ", testDate)).toBe(
-      "2022-06-17T05:24:12-05:00"
+      "2022-06-17T05:24:12+00:00"
     );
   });
 
-  test("throw error typeerror when receiving number instead of string", () => {
+  it("should format date to ISOTime (hh:mm:ss)", () => {
     expect(unitTestingTask("ISOTime", testDate)).toBe("05:24:12");
   });
 
-  test("throw error typeerror when receiving number instead of string", () => {
+  it("should typeerror when receiving number instead of string in format parameter", () => {
     expect(() => unitTestingTask(10, testDate)).toThrow(TypeError);
   });
 
-  test("throw error typeerror when non valid date", () => {
+  it("should throw typeerror when non valid date", () => {
     expect(() => unitTestingTask("ZZ", null)).toThrow(TypeError);
   });
 });
